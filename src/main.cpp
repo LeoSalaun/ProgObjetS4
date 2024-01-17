3323c662a92458895e91660a648c6825dc679eab
LeoSalaun <salaunleo29@gmail.com>
2024-01-17 16:16:45 +0100

    First try at p6

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,13 @@ int main()
         return EXIT_FAILURE;
 
     // Actual application code
-    auto ctx = p6::Context{{.title = "Simple-p6-Setup"}};
+    auto ctx = p6::Context{{.title = "prog-web-s4"}};
     ctx.maximize_window();
 
+    ctx.background(p6::NamedColor::Jade);
     // Declare your infinite update loop.
     ctx.update = [&]() {
-        ctx.background(p6::NamedColor::Blue);
+        //ctx.background(p6::NamedColor::Jade);
         ctx.circle(
             p6::Center{ctx.mouse()},
             p6::Radius{0.2f}
