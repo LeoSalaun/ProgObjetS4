439bee6133b72e8607e7a062a0f078d4fe2fe36f
LeoSalaun <salaunleo29@gmail.com>
2024-02-07 18:01:24 +0100

    Jules le goat

--- src/Boid.cpp
+++ src/Boid.cpp
@@ -1,28 +1,36 @@
 #include "Boid.hpp"
+#include "glm/fwd.hpp"
 #include "p6/p6.h"
 #include <iostream>
 #include <time.h>
 
 Boid::Boid()
-: position(Boid::DIMENSION), direction(Boid::DIMENSION) {
+: position(), direction() {
     srand(time(NULL));
-    color = {static_cast<float>(rand()%256)/256 , static_cast<float>(rand()%256)/256 , static_cast<float>(rand()%256)/256};
-    for (int i=0 ; i<Boid::DIMENSION ; i++) {
-        direction[i] = static_cast<float>(rand()%100-50)/10000;
-    }
+    color = glm::vec3(static_cast<float>(rand()%256)/256 , static_cast<float>(rand()%256)/256 , static_cast<float>(rand()%256)/256);
+    direction = vec(static_cast<float>(rand()%100-50)/10000);
 }
 
 void Boid::display(p6::Context &ctx) {
     ctx.circle(
-            p6::Center{position[0],position[1]},
-            p6::Radius{0.2f}
-        );
+        p6::Center{position[0],position[1]},
+        p6::Radius{0.2f}
+    );
 }
 
 void Boid::updatePosition() {
-    for (int i=0 ; i<Boid::DIMENSION ; i++) {
-        direction[i] = static_cast<float>(rand()%100-50)/1000;
-        position[i] += direction[i];
-    
+        direction = vec(static_cast<float>(rand()%100-50)/1000);
+        position += direction;
 }
+
+vec Boid::getPosition() const {
+    return position;
+}
+
+glm::vec3 Boid::getColor() const {
+    return color;
+}
+
+vec Boid::getDirection() const {
+    return direction;
 }
